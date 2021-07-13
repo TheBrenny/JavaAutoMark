bca564f7644820120ee4c0692260f759ec096a02
TheBrenny <iam+dev@justbrenny.me>
2021-07-13 14:59:55 +1000

    Initial commit

---
+++ src/edu/group5/jam/Main.java
@@ -0,0 +1,15 @@
+package edu.group5.jam;
+
+import javax.swing.JFrame;
+
+public class Main {
+    private final JFrame frame;
+    
+    public Main() {
+        this.frame = new JFrame("JAM - Java Auto Marker");
+    }
+
+    public JFrame getFrame() {
+        return this.frame;
+    }
+}
