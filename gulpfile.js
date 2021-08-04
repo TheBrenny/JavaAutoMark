68cbb959cc71c54bddce68fe47dab43917a2d1f8
TheBrenny <iam+dev@justbrenny.me>
2021-08-05 07:28:28 +1000

    fixed dev install, and added sourcemaps to gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon');
+
+const host = "jam";
+const port = 80;
+
+gulp.task("sass", function () {
+    return gulp.src("app/assets/scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass().on("error", sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("app/assets/css/"))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task("browserSync", function (cb) {
+    return browserSync.init({
+        proxy: "http://" + host + "/",
+        files: ["app/assets/**/*.*", "app/views/**/*.*"],
+        ignore: ["app/assets/scss/**/*.*"],
+        open: false,
+        port: port + 1
+    }, cb);
+});
+
+gulp.task("nodemon", function (cb) {
+    var started = false;
+
+    return nodemon({
+        script: 'server.js',
+        env: {
+            "NODE_ENV": 'dev',
+            "GULPING": true,
+            "HOST": host,
+            "PORT": port
+        },
+        watch: ["*.js", "app/routes/**/*.*"]
+    }).on('start', function () {
+        // to avoid nodemon being started multiple times
+        // thanks @matthisk
+        if (!started) {
+            started = true;
+            console.log("Nodemon started.");
+            cb();
+        }
+    });
+});
+
+gulp.task("watch", gulp.series("sass", function (cb) {
+    gulp.watch("app/assets/scss/**/*.scss", gulp.series("sass"));
+    cb();
+}));
+
+gulp.task("default", gulp.series("nodemon", "browserSync", "watch"));
