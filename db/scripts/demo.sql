c51d0fa53d5f680a99a3f4a038fa57d146053f5f
TheBrenny <iam+dev@justbrenny.me>
2021-08-20 23:13:26 +1000

    query formatting

--- db/scripts/demo.sql
+++ db/scripts/demo.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (`id`, `username`, `password`, `active`) VALUES       
-    (1, 'Jack', '$2b$12$fLZ/YIkdvefrVXtA2bOxI.MHpXtylXWmHoVmDF.S9fGJkjrtucuBm', 1); -- Password is Henry
+INSERT INTO `teacher` (`zid`, `email`, `fname`, `lname`, `password`) VALUES       
+    (5217759, 'z5217759@ad.unsw.edu.au', 'Jarod', 'Brennfleck', '$2b$12$nBqcI4CEVdSEam8iELnJf.jXvM.oXV7BU0Zb7OAnJdrXHAAfDoY4q'); -- Password is Brennfleck
--- db/scripts/queries.sql
+++ db/scripts/queries.sql
@@ -1,3 +1,5 @@
+-- INSTALL COMMANDS EXTRACTED TO INSTALL.SQL
+
 -- Insert teacher into Teacher Table
 INSERT INTO Teacher (`zID`, `email`, `fname`, `lname`, `password`) VALUES       
     (5217759, 'z5217759@ad.unsw.edu.au', 'Jarod', 'Brennfleck', '$2b$12$nBqcI4CEVdSEam8iELnJf.jXvM.oXV7BU0Zb7OAnJdrXHAAfDoY4q'); -- Password is Brennfleck
