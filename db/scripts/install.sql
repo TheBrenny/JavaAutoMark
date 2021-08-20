fb839214f488573a4b4a3221b12b14ff7b3f32e5
TheBrenny <iam+dev@justbrenny.me>
2021-08-20 23:08:03 +1000

    extracted install queries

--- db/scripts/install.sql
+++ db/scripts/install.sql
@@ -1,27 +1,38 @@
-CREATE TABLE `users` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(40) NOT NULL,
-  `password` CHAR(60) NOT NULL,
-  `active` BOOLEAN NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id`)
+-- Create Teacher Table
+CREATE TABLE `teacher` (
+  `zid` MEDIUMINT UNIQUE NOT NULL,
+  `email` VARCHAR(320) NOT NULL,
+  `fname` VARCHAR(25) NOT NULL,
+  `lname` VARCHAR(25) NOT NULL,
+  password CHAR(60) NOT NULL,
+  PRIMARY KEY (`zid`)
 );
 
--- %%%% Comes from the 'express-mysql-session' package %%%%
--- $$$$                                                $$$$
--- CREATE TABLE IF NOT EXISTS `sessions` (
---   `session_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
---   `expires` int(11) unsigned NOT NULL,
---   `data` mediumtext COLLATE utf8mb4_bin,
---   PRIMARY KEY (`session_id`)
--- ) ENGINE=InnoDB
+-- Create Course Table
+CREATE TABLE `course` (
+  `course_uuid` INTEGER AUTO_INCREMENT UNIQUE,
+  `course_id` CHAR(8) NOT NULL,
+  `course_name` VARCHAR(100) NOT NULL,
+  `running_year` SMALLINT NOT NULL,
+  PRIMARY KEY (`course_id`, `running_year`)
+);
 
--- %%%% Comes from /app/routes/rememberme.js %%%%
--- $$$$                                      $$$$
--- CREATE TABLE IF NOT EXISTS `rememberTokens` (
---   `id` INTEGER NOT NULL AUTO_INCREMENT,
---   `selector` CHAR(40) NOT NULL,
---   `validator` CHAR(60) NOT NULL,
---   `user` INTEGER NOT NULL,
---   PRIMARY KEY (`id`),
---   FOREIGN KEY (`user`) REFERENCES `users` (`id`)
--- );
+-- Create Assignmnet Table
+CREATE TABLE `assignment` (
+  `assignment_id` INTEGER AUTO_INCREMENT UNIQUE,
+  `course_uuid` INTEGER NOT NULL,
+  `code_location` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`assignment_id`),
+  FOREIGN KEY (`course_uuid`) REFERENCES `course` (`course_uuid`)
+);
+
+-- Create Report Table 
+CREATE TABLE `report` (
+  `report_id` INTEGER AUTO_INCREMENT UNIQUE,
+  `zid` MEDIUMINT UNIQUE NOT NULL,
+  `student_name` VARCHAR(50) NOT NULL,
+  `report_location` VARCHAR(255) NOT NULL,
+  `assignment_id` INTEGER NOT NULL,
+  PRIMARY KEY (`zid`, `assignment_id`),
+  FOREIGN KEY (`assignment_id`) REFERENCES `assignment` (`assignment_id`)
+);
