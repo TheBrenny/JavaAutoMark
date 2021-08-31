f304a6605666c2ea2b2616ee9524ff812a4bd764
TheBrenny <iam+dev@justbrenny.me>
2021-09-01 09:10:42 +1000

    updated how errors work

--- app/routes/errors/404.js
+++ app/routes/errors/404.js
@@ -4,10 +4,6 @@ class Error404 extends ErrorGeneric {
     constructor(method, url) {
         super(404, "Not Found", `Could not ${method.toUpperCase()} ${url}`);
     }
-
-    static fromReq(req) {
-        return new Error404(req.method, req.url);
-    }
 }
 
 module.exports = Error404;
--- app/routes/errors/generic.js
+++ app/routes/errors/generic.js
@@ -5,9 +5,14 @@ class ErrorGeneric extends Error {
         this.status = code;
         this.name = name;
     }
+
+    static fromReq(req) {
+        return new(this.prototype.constructor)(req.method, req.url);
+    }
 }
 
 module.exports = ErrorGeneric;
 module.exports.errors = {
-    404: require("./404")
+    404: require("./404"),
+    501: require("./501")
 };
