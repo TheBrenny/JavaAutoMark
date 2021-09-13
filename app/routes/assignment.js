986230225693d8a03d82b518ed20b4dba873c510
rchenginiyadan <55564912+rchenginiyadan@users.noreply.github.com>
2021-09-13 12:31:31 +1000

    Code for converting .txt to .java

--- app/routes/assignment.js
+++ app/routes/assignment.js
@@ -3,6 +3,7 @@ const checks = require("./tools/checks");
 const session = require("./tools/session");
 const Database = require("../../db/database");
 const errors = require("./errors/generic").errors;
+const storage = require("../../storage/storage");
 
 router.get("/assignments", async (req, res) => {
     throw errors.notImplemented.fromReq(req);
@@ -36,19 +37,30 @@ router.post("/assignments/create", async (req, res) => {
     let assignment = req.body;
     
     let code = `public class ${assignment.name} {\n`;
-    code += `public void task1() {\n`;
-    code += `${assignment.tasks[0].tests[0].code}`;
+    
+    // for loop for all tasks in the assignment
+    for (let i = 0; i < assignment.tasks.length; i++) {
+        code += `public void ${assignment.tasks[0]} {\n`;
+        code += `${assignment.tasks[0].tests[0].code}`;
 
-    code += `System.out.println(${assignment.tasks[0].tests[0].code})`;
-    code += `System.out.println(${assignment.tasks[0].tests[0].code}.equals(${assignment.tas.asd.expected}))`;
+        code += `System.out.println(${assignment.tasks[0].tests[0].code})`;
+        code += `System.out.println(${assignment.tasks[0].tests[0].code}.equals(${assignment.tasks[0].tests[0].expected}))`; 
+    }
 
-    let id = (await Daabase.assignments.addAssignment(assignment.class, "somehwere")).id;
+
+    let path = assignment.class + "/" + assignmen.year + "/" + assignment.name;
+
+    await Database.assignments.addAssignment(assignment.class, path);
+    await storage.putObject(storage.container, path + ".java", code);
+    await storage.putObject(storage.container, path + ".json", assignment);
+
+    let id = (await Database.assignments.addAssignment(assignment.class, "somehwere")).id;
     res.setHeader("Location", "/assignments/edit/" + id);
     res.status(201).end();
 });
 
-router.get("/assignments/edit/:id", (req, res) => {
-    throw errors.notImplemented.fromReq(req);
-});
+//router.get("/assignments/edit/:id", (req, res) => {
+ //   throw errors.notImplemented.fromReq(req);
+//});
 
 module.exports = router;
