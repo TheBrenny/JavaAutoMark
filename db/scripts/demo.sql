0b1ec0faff087c15ed9480e73c9884e3677f05aa
TheBrenny <iam+dev@justbrenny.me>
2021-09-15 16:19:45 +1000

    updated demo data

--- db/scripts/demo.sql
+++ db/scripts/demo.sql
@@ -1,6 +1,7 @@
-INSERT INTO `teachers` (`zid`, `email`, `fname`, `lname`, `password`) VALUES       
-    (5217759, 'z5217759@ad.unsw.edu.au', 'Jarod', 'Brennfleck', '$2b$12$nBqcI4CEVdSEam8iELnJf.jXvM.oXV7BU0Zb7OAnJdrXHAAfDoY4q'), -- Password is Brennfleck
-    (5260786, 'z5260786@ad.unsw.edu.au', 'Ethan', 'Fraser', '$2b$12$WDDGEe8l8eZsK3H4tdr6/OrzPKhEmvI/0njJX9OlfqhMpZsVjvuxK'); -- Password is Fraser
+INSERT INTO teachers ('zid', 'email', 'fname', 'lname', 'password') VALUES
+    (5217759, 'j.b@email.com', 'Jarod', 'Brennfleck', '$2b$12$nBqcI4CEVdSEam8iELnJf.jXvM.oXV7BU0Zb7OAnJdrXHAAfDoY4q'), -- Password is Brennfleck
+    (5260727, 'r.g@email.com', 'Roshan', 'George', '$2b$12$jt7fyJYcGnYHgWgNEfY5cuMyBqdqk0Cij4CVhzR2euCbKyKKnuW1G'), -- Password is test
+    (5260786, 'e.f@email.com', 'Ethan', 'Fraser', '$2b$12$WDDGEe8l8eZsK3H4tdr6/OrzPKhEmvI/0njJX9OlfqhMpZsVjvuxK'); -- Password is Fraser
 
 INSERT INTO `courses` (`course_id`, `course_name`, `running_year`) VALUES
     ('ZEIT3101', 'IT Project 2', '2021'),
