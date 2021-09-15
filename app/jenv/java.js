83e5c677b37cd98f031411c3d9387ec6a2819c6f
TheBrenny <iam+dev@justbrenny.me>
2021-09-15 16:20:07 +1000

    re-started jenv work

---
+++ app/jenv/java.js
@@ -0,0 +1,21 @@
+const {
+    spawn
+} = require("child_process");
+const path = require("path");
+const config = require("../../config");
+
+const java = config.java.java;
+const compiler = config.java.compiler;
+
+function run(target) {
+
+}
+
+function compile(target) {
+
+}
+
+module.exports = {
+    run,
+    compile
+};
