ad13d5f89581c49d5ce0bcb1af751cd96fab7bdb
MrDoesNotUnderstand <72126684+MrDoesNotUnderstand@users.noreply.github.com>
2021-10-07 16:05:22 +1100

    fix bad reference to creating courses

--- app/assets/js/courses.js
+++ app/assets/js/courses.js
@@ -1,20 +1,16 @@
 (() => {
-    console.log("We are in the file");
     var form = document.getElementById("createCourse");
     form.onsubmit = validateForm;
-
+    form.onsubmit = validateCourseID;
+    form.onsubmit = validateCourseYear;
+    
     function validateForm(){
         let  courseID = form['id'].value;
         let courseName = form['name'].value;
         let courseYear = form['year'].value;
-        let courseCodeValid = courseID.indexOf("Z") == 0 || courseID.indexOf("z") == 0;
-        
+
         if (courseID == ""){
             alert("Please enter a Course Code");
-            return false;
-        }
-        if(!courseCodeValid){
-            alert("Please Enter a valid Course Code");
             return false;
         }
         if (courseName == ""){
@@ -23,6 +19,36 @@
         }
         if(courseYear == ""){
             alert("Please enter a Course Year");
+            return false;
+        }
+        return true;
+    }
+
+    function validateCourseID(){
+        let  courseID = form['id'].value;
+        let courseNumbers = courseID.substring(courseID.length-4, courseID.length);
+        let courseLetters = courseID.substring(0, 4);
+
+        let condition1 = courseID.indexOf("Z") == 0 || courseID.indexOf("z") == 0;
+        let condition2 = courseNumbers.match(/^[0-9]+$/) != null;
+        let condition3 = courseLetters.match(/^[a-z]+$/i) != null;
+        let condition4 = courseID.length == 8;
+
+        if(!(condition1 && condition2 && condition3 && condition4)){
+            alert("Please enter a valid course code, eg. ZEIT2306");
+            return false;
+        }
+        return true;
+    }
+
+    function validateCourseYear(){
+        let courseYear = form['year'].value;
+
+        condition1 = courseYear.match(/^[0-9]+$/) != null;
+        condition2 = courseYear.length == 4;
+
+        if(!(condition1 && condition2)){
+            alert("Please enter a valid year");
             return false;
         }
         return true;
