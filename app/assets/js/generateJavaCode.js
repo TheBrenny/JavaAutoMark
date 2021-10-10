cb6a5b510db2e909e6a2134b876bec432c472a90
TheBrenny <iam+dev@justbrenny.me>
2021-10-10 17:10:53 +1100

    allow assignment class to control sysout

--- app/assets/js/generateJavaCode.js
+++ app/assets/js/generateJavaCode.js
@@ -3,32 +3,61 @@
 
 function generateJavaCode(assignment, assignmentID) {
     if(assignmentID === null || assignmentID === undefined) throw new Error("assignmentID must be specified");
-    let code = `public class Assignment${assignmentID} {\n`;
+    let code = ``;
+    code += `import java.io.PrintStream;\n`;
+    code += `import java.io.OutputStream;\n`;
+    code += `public class Assignment${assignmentID} {\n`;
 
     // for loop for all tasks in the assignment
     let main = "public static void main(String[] args) {\n";
+
+    // This gives us control over the output stream because we only want to println our output
+    main += `// This gives us control over the console output stream because we only want to println our harness output\n`;
+    main += `PrintStream sysout = System.out;\n`;
+    main += `System.setOut(new PrintStream(new OutputStream() {\n`;
+    main += `    public void write(int b) {\n`;
+    main += `        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();\n`;
+    // main += `        for(int i = 0; i < stackTrace.length; i++) {\n`;
+    // main += `            sysout.println(i + " - " + stackTrace[i].getClassName());\n`;
+    main += `            if(stackTrace[stackTrace.length-3].getClassName().equals(PrintStream.class.getName())) {\n`; // testing indicates that st[st.length - 3] should always be printstream
+    main += `                sysout.write(b);\n`;
+    main += `            }\n`;
+    // main += `        }\n`;
+    main += `    }\n`;
+    main += `}));\n`;
+    main += `\n`;
+
     for(let i = 0; i < assignment.tasks.length; i++) {
+        let task = assignment.tasks[i];
         main += `task${i + 1}();\n`;
         code += `public static void task${i + 1}() {\n`;
+        code += `System.out.println("task.start.${task.taskID}");\n`;
 
-        for(let j = 0; j < assignment.tasks[i].tests.length; j++) {
-            let insertCode = assignment.tasks[i].tests[j].code;
+        for(let j = 0; j < task.tests.length; j++) {
+            let test = task.tests[j];
+            let insertCode = test.code;
             insertCode = insertCode.replace(/\r\n/gm, "\n").trim(); // strip crlf to just lf
 
-            if(assignment.tasks[i].tests[j].testID !== undefined) {
-                let expected = assignment.tasks[i].tests[j].expected;
-                if(assignment.tasks[i].tests[j].isException) {
+            if(test.testID !== undefined) {
+                let expected = test.expected;
+                code += `System.out.println("test.start.${task.taskID}.${test.testID}");\n`;
+                if(test.isException) {
                     code += `try {\n`;
                     code += `${insertCode}\n`;
                     code += `} catch (Exception e) {\n`;
+                    code += `System.out.println("test.output.actual.${task.taskID}.${test.testID}");\n`;
                     code += `System.out.println(e.getClass().getSimpleName());\n`;
+                    code += `System.out.println("test.output.passed.${task.taskID}.${test.testID}");\n`;
                     code += `System.out.println("${expected}" == "" ? true : "${expected}".equals(e.getClass().getSimpleName()));\n`;
                     code += `}\n`;
                 } else {
                     insertCode.replace(/;$/gm, ""); // strip trailing semicolon
+                    code += `System.out.println("test.output.actual.${task.taskID}.${test.testID}");\n`;
                     code += `System.out.println(${insertCode});\n`;
+                    code += `System.out.println("test.output.passed.${task.taskID}.${test.testID}");\n`;
                     code += `System.out.println(((Object) ${insertCode}).equals(${expected}));\n`; // Object casting is needed to compare primitives
                 }
+                code += `System.out.println("test.end.${task.taskID}.${test.testID}");\n`;
             } else {
                 code += `${insertCode}\n`;
             }
@@ -37,13 +66,13 @@ function generateJavaCode(assignment, assignmentID) {
                 code: insertCode // save the sanitised code back
             });
         }
-
+        code += `System.out.println("task.end.${task.taskID}");\n`;
         code += `}\n`;
     }
 
     main += "}\n";
-    code += main;
 
+    code += main;
     code += `}\n`;
 
     return code;
